2a982a2c47f95f71ead2d697a03c21f7a7b4a2a8
Francisco Shaban <franmarketing.uy@gmail.com>
2022-09-03 22:10:45 -0300

    last commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -7,16 +7,16 @@ const nuevaString = "Hola";
 const nuevoNum = 77
 
 // Crea una variable booleana:
-const nuevoBool = null;
+const nuevoBool = Copalibertadores = true 
 
 // Resuelve el siguiente problema matemático:
-const nuevaResta = null;
+const nuevaResta = 25 - 10 === 15;
 
 // Resuelve el siguiente problema matemático:
-const nuevaMultiplicacion = 10 * null === 40 ;
+const nuevaMultiplicacion = 10 * 4 === 40 ;
 
 // Resuelve el siguiente problema matemático:
-const nuevoModulo = 21 % 5 === null;
+const nuevoModulo = 21 % 5 === 1
 
 
 // En los próximos 22 problemas, deberás completar la función.
